81556439153955bdb41b8622a257878d5acab6e1
Codey Pinero <codey.pinero@gmail.com>
2019-04-03 23:31:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 
 # Add  code here!
 def prime?(num)
-  if num < 0 || num == 0 || num == 1
-    false
-  else (2..num-1).to_a.all? do |factor|
-    num % factor != 0
+    if num < 0 || num == 0 || num == 1
+      false
+    else (2..num-1).to_a.all? do |factor|
+      num % factor != 0
     end
   end
 end
